c9a50913bc18a4e010efcf00b6bcdd15e7885fb9
Joao Vinicius <jvgds03@gmail.com>
2026-04-22 21:35:48 -0300

    first commit

---
+++ prw3_2026_1_api/src/main/java/br/edu/ifsp/prw3/prw3_2026_1_api/Prw320261ApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.prw3.prw3_2026_1_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prw320261ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prw320261ApiApplication.class, args);
+	}
+
+}
---
+++ prw3_2026_1_api/src/test/java/br/edu/ifsp/prw3/prw3_2026_1_api/Prw320261ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.prw3.prw3_2026_1_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prw320261ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
